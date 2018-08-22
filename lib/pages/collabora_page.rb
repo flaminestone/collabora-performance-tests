a6a1d0ed5c8bf4cf78e2bd7b1b904ef3ee065c52
Dmitriy Rotatyy <flamine@list.ru>
2018-08-22 12:52:11 +0300

    Increace porform

--- lib/pages/collabora_page.rb
+++ lib/pages/collabora_page.rb
@@ -4,10 +4,10 @@ class CollaboraPage
 
   def write_text(instance)
     @instance = instance
-    100.times do |i|
+    500.times do |i|
       puts "#{Time.now} ------- #{i}"
-      @instance.send_keys('1 2 3 4 5 6 7 8 9 0', '1 2 3 4 5 6 7 8 9 0')
-      sleep 3
+      @instance.send_keys('', '1234567890')
+      sleep 2
     end
   end
 end
